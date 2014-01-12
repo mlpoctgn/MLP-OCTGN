52124c5314c94b69bad72c9bb8efda8c4dc252a9
Gabby Gums <mlpoctgn@gmail.com>
2014-01-12 13:20:47 -0700

    Release Version 2.1.1.0
    Updated Inspired ability, added to Promo Set, fixed card typos

--- scripts/actions.py
+++ scripts/actions.py
@@ -30,89 +30,121 @@ def sixSided(group, x = 0, y = 0):
 	notify("{} rolls a {} on a 6-sided die.".format(me, n))
 
 def inspired(targetgroup, x = 0, y = 0, count = None):
-    mute()
-    if len(players) == 1:
-    	notify("There must be more than 1 player to use Inspired.")
-    	return
-    
-    group = players[1].Deck
-    	
-    inspiredCount = sum(1 for card in table if card.controller == me and re.search(r'Inspired', card.Keywords))
-    
-    if count == None:
-        count = askInteger("Look at how many cards?", inspiredCount)
-    if count == None or count == 0:
-        return
-    
-    notify("{} uses Inspired to look at the top {} cards of {}'s deck.".format(me, count, players[1]))
-    
-    topCards = group.top(count)
-
-    buttons = []  ## This list stores all the card objects for manipulations.
-    for c in topCards:
-        c.peek()  ## Reveal the cards to python
-        buttons.append(c)
-       
-    topList = []  ## This will store the cards selected for the top of the pile
-    bottomList = []  ## For cards going to the bottom of the pile
-    rnd(1,2)  ## allow the peeked card's properties to load
-    loop = 'BOTTOM'  ## Start with choosing cards to put on bottom
-           
-    while loop != None:
-        desc = "Select a card to place on {}:\n\n{}\n///////DECK///////\n{}".format(
-                loop,
-                '\n'.join([c.Name for c in topList]),
-                '\n'.join([c.Name for c in bottomList]))
-	if loop == 'TOP':
-		num = askChoice(desc, [c.Type + ": " + c.Name + " " + c.Subname for c in buttons], customButtons = ["Select BOTTOM","Leave Rest on BOTTOM","Reset"])
-		if num == -1:
-			loop = 'BOTTOM'         
-		elif num == -2:
-			while len(buttons) > 0:
-				card = buttons.pop()
-				bottomList.append(card)
-		elif num == -3:
-			topList = []
-			bottomList = []
-			buttons = []
-			for c in group.top(count):
-				c.peek()
-				buttons.append(c)
-		elif num > 0:
-			card = buttons.pop(num - 1)
-			topList.insert(0, card)
-	else:
-		num = askChoice(desc, [c.Type + ": " + c.Name + " " + c.Subname for c in buttons], customButtons = ["Select TOP","Leave Rest on TOP","Reset"])
-		if num == -1:
-			loop = 'TOP'
-		elif num == -2:
-			while len(buttons) > 0:
-				card = buttons.pop()
-				topList.insert(0, card)
-		elif num == -3:
-			topList = []
-			bottomList = []
-			buttons = []
-			for c in group.top(count):
-				c.peek()
-				buttons.append(c)
-		elif num > 0:
-			card = buttons.pop(num - 1)
-			bottomList.append(card)
-	if len(buttons) == 0: ##  End the loop
-		loop = None
-	if num == None:  ## closing the dialog window will cancel the ability, not moving any cards, but peek status will stay on.
+	mute()
+	if len(players) == 1:
+		notify("There must be more than 1 player to use Inspired.")
 		return
-    topList.reverse()  ## Gotta flip topList so the moveTo's go in the right order
-    for c in topList:
-        c.moveTo(group)
-    for c in bottomList:
-        c.moveToBottom(group)
-    for c in group:  ## This removes the peek status
-        c.isFaceUp = True
-        c.isFaceUp = False
-    notify("{} looked at {} cards and put {} on top and {} on bottom.".format(me, count, len(topList), len(bottomList)))
+	
+	group = players[1].Deck
+	
+	inspiredCount = sum(1 for card in table if card.controller == me and re.search(r'Inspired', card.Keywords))
+	
+	if count == None:
+		count = askInteger("Look at how many cards?", inspiredCount)
+	if count == None or count == 0:
+		return
+	
+	notify("{} uses Inspired to look at the top {} cards of {}'s deck.".format(me, count, players[1]))
+	
+	topCards = group.top(count)
+	
+	buttons = []  ## This list stores all the card objects for manipulations.
+	for c in topCards:
+		c.peek()  ## Reveal the cards to python
+		buttons.append(c)
+	
+	topList = []  ## This will store the cards selected for the top of the pile
+	bottomList = []  ## For cards going to the bottom of the pile
+	rnd(1,2)  ## allow the peeked card's properties to load
+	loop = 'BOTTOM'  ## Start with choosing cards to put on bottom
+	
+	while loop != None:
+		desc = "Select a card to place on {}:\n\n{}\n///////DECK///////\n{}".format(
+		loop,
+		'\n'.join([c.Name for c in topList]),
+		'\n'.join([c.Name for c in bottomList]))
+		if loop == 'TOP':
+			num = askChoice(desc, ["(" + c.Power + ") " + c.Type + ": " + c.Name + " - " + c.Subname for c in buttons], customButtons = ["Select BOTTOM","Leave Rest on BOTTOM","Reset"])
+			if num == -1:
+				loop = 'BOTTOM'         
+			elif num == -2:
+				while len(buttons) > 0:
+					card = buttons.pop()
+					bottomList.append(card)
+			elif num == -3:
+				topList = []
+				bottomList = []
+				buttons = []
+				for c in group.top(count):
+					c.peek()
+					buttons.append(c)
+			elif num > 0:
+				card = buttons.pop(num - 1)
+				topList.insert(0, card)
+		else:
+			num = askChoice(desc, ["(" + c.Power + ") " + c.Type + ": " + c.Name + " - " + c.Subname for c in buttons], customButtons = ["Select TOP","Leave Rest on TOP","Reset"])
+			if num == -1:
+				loop = 'TOP'
+			elif num == -2:
+				while len(buttons) > 0:
+					card = buttons.pop()
+					topList.insert(0, card)
+			elif num == -3:
+				topList = []
+				bottomList = []
+				buttons = []
+				for c in group.top(count):
+					c.peek()
+					buttons.append(c)
+			elif num > 0:
+				card = buttons.pop(num - 1)
+				bottomList.append(card)
+		if len(buttons) == 0: ##  End the loop
+			loop = None
+		if num == None:  ## closing the dialog window will cancel the ability, not moving any cards, but peek status will stay on.
+			return
 
+	topList.reverse()  ## Gotta flip topList so the moveTo's go in the right order
+	
+	originalOwner = group.controller
+
+	group.setController(me)
+
+	update()
+
+	for c in topList:
+		c.setController(me)
+
+	update()
+
+	for c in topList:
+		c.moveTo(group,0)
+
+	update()
+	
+	for c in topList:
+		c.setController(originalOwner)
+	
+	update()
+	
+	for c in bottomList:
+		c.moveToBottom(group)
+		
+	update()
+		
+	for c in group:  ## This removes the peek status
+		c.isFaceUp = True
+		c.isFaceUp = False
+	
+	update()
+
+	group.setController(originalOwner)
+
+	update()
+	
+	whisper("{}".format(group.controller))
+
+	notify("{} looked at {} cards and put {} on top and {} on bottom.".format(me, count, len(topList), len(bottomList)))
 
 #def inspiredPeek(card, x = 0, y = 0):
 #	if len(players) == 1: return
